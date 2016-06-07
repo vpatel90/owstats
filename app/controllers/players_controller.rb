92b16a2691b2ad2eab1ecb3706b004e47846e8e5
Vivek Patel <vnp229@gmail.com>
2016-06-07 13:42:34 -0500

    stats being stored in db now

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,11 +9,15 @@ class PlayersController < ApplicationController
     response = HTTParty.get("https://owapi.net/api/v1/u/#{params[:tag]}/stats")
     player = Player.find_by(tag: params[:tag])
     if response["battletag"]
-      player = Player.create(tag: params[:tag]) unless player
+      player = create_new_player unless player
       player.store_data(response)
       {message: "Stored Success"}
     else
       {message: "Unable to get a Response due to invalid battletag"}
     end
+  end
+
+  def create_new_player
+    Player.create(tag: params[:tag])
   end
 end
