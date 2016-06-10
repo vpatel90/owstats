df417f088653d47f2ac91cc0fcd1b21a9dc4f26f
Vivek Patel <vnp229@gmail.com>
2016-06-10 12:00:42 -0500

    now when a player is created, it creats empty player_hero containers and each player_hero also has empty containers for varios hero stat models, not the actual stats

--- app/models/player_hero.rb
+++ app/models/player_hero.rb
@@ -1,4 +1,19 @@
 class PlayerHero < ActiveRecord::Base
   belongs_to :hero
   belongs_to :player
+  has_one :best_game, dependent: :destroy
+  has_one :best_life, dependent: :destroy
+  has_one :hero_per_game, dependent: :destroy
+  has_one :hero_total, dependent: :destroy
+  has_one :specific_stat, dependent: :destroy
+
+  after_create :create_init_stat_containers
+
+  def create_init_stat_containers
+    BestGame.create(player_hero_id: self.id)
+    BestLife.create(player_hero_id: self.id)
+    HeroPerGame.create(player_hero_id: self.id)
+    HeroTotal.create(player_hero_id: self.id)
+    SpecificStat.create(player_hero_id: self.id)
+  end
 end
